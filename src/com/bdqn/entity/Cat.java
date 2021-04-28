11d313c41a21c42d094261519d732365688fec9b
bxl_glb <goodMorning_pro@at.bdqn.com>
2021-04-28 15:31:50 +0800

    ZXD提交文件

---
+++ src/com/bdqn/entity/Cat.java
@@ -0,0 +1,19 @@
+package com.bdqn.entity;
+
+public class Cat {
+	private int id;  //id
+	private String carName;   //猫猫的名字
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCarName() {
+		return carName;
+	}
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+}
